08fa75d08f16ce9a55dc41f858a3694a609f9cbd
readera193 <readera193@gmail.com>
2020-09-04 15:16:56 +0800

    chore: 安裝 ide-helper
    安裝 ide-helper

--- config/app.php
+++ config/app.php
@@ -160,6 +160,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Ide-Helper
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
