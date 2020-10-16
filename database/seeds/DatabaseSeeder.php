efff58d44a9fe8ab2a7b8f4fb03e9236a9a756bd
readera193 <readera193@gmail.com>
2020-10-16 18:07:06 +0800

    feat: 建立產品的資料表
    建立產品 controller, view, model, migration, seeder
    使用 products.sql 匯入資料
    
    issue #1

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,10 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
 
         DB::unprepared(file_get_contents('database/products.sql'));
-<<<<<<< HEAD
         $this->command->info('products table seeded');
-=======
-        $this->command->info('products table seeded!');
->>>>>>> parent of 3bc6b21... Revert "feat: 建立產品的資料表"
     }
 }
